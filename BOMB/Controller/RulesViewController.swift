039be1803dc1a2084f18a7c276efe26c11d6295b
Daria Sotnikova <dasha.dasha5454@yandex.ru>
2023-08-11 01:24:04 +0300

    dynamic screen build

--- BOMB/Controller/RulesViewController.swift
+++ BOMB/Controller/RulesViewController.swift
@@ -11,152 +11,173 @@ class RulesViewController: UIViewController {
     
     let scrollView = UIScrollView()
     let contentView = UIView()
+    let model = RulesModel()
     
-    lazy var rulesTitleLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.text = "Правила Игры"
-        label.textColor = UIColor(named: "purple")
-        label.font = .boldSystemFont(ofSize: 40)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+        lazy var rulesTitleLabel: UILabel = {
+            let label = UILabel(frame: .zero)
+            label.text = "Правила Игры"
+            label.textColor = UIColor(named: "purple")
+            label.font = .boldSystemFont(ofSize: 40)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
     
-    lazy var rulesHelpLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        
-        label.font = .boldSystemFont(ofSize: 22)
-        label.textAlignment = .center
-        label.text = """
-1 Все игроки становятся в круг.
-
-2 Первый игрок берет телефон и нажимает кнопку:
-
-3 На экране появляется вопрос. Например, «Назовите фрукт».
-
-4 Игрок отвечает на вопрос и после правильного ответа передает телефон следующему игроку (правильность ответа определяют другие участники).
-
-5 Игроки по кругу отвечают на один и тот же вопрос до тех пор, пока не взорвется бомба.
-
-6 Проигравшим считается тот, в чьих руках взорвалась бомба.
-
-7 Если в настройках выбран режим игры «С заданиями», то проигравший выполняет задание.
-"""
-        return label
-    }()
+        lazy var starButtonImage: UIImageView = {
+            let image = UIImageView(frame: .zero)
+            image.translatesAutoresizingMaskIntoConstraints = false
+            image.image = UIImage(named: "start-game-rules")
+            return image
+        }()
     
-    lazy var categoriesTitleLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.text = "Категории"
-        label.textColor = UIColor(named: "purple")
-        label.font = .boldSystemFont(ofSize: 40)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+        lazy var categoriesTitleLabel: UILabel = {
+            let label = UILabel(frame: .zero)
+            label.text = "Категории"
+            label.textColor = UIColor(named: "purple")
+            label.font = .boldSystemFont(ofSize: 40)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+   
+        func configureNavBar() {
+            navigationItem.title = "Помощь"
+            navigationController?.navigationBar.barStyle = .default
+//            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.purple]
+            navigationController?.navigationBar.backgroundColor = .white
+            navigationController?.navigationBar.barTintColor = .white
+            navigationController?.navigationBar.isTranslucent = false
     
-    lazy var categoriesHelpLabel: UILabel = {
-        var label = UILabel(frame: .zero)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        label.font = .boldSystemFont(ofSize: 22)
-        label.textAlignment = .center
-        label.text = "В игре доступно 5 категорий по 15 вопросов.\n\n\nМожно выбрать сразу несколько категорий для игры."
-        return label
-    }()
+            navigationItem.backBarButtonItem?.tintColor = .black
     
-    lazy var categoriesContainer: UIView = {
-        let view = UIView(frame: .zero)
-        view.backgroundColor = .red
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
-    
-    lazy var category: UIView = {
-        let view = UIView(frame: .zero)
-        view.backgroundColor = .purple
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 15
-        return view
-    }()
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        scrollView.backgroundColor = .white
-              
         configureNavBar()
-        setupScrollView()
-    }
-    
-    func configureNavBar() {
-        navigationItem.title = "Помощь"
-        navigationController?.navigationBar.barStyle = .default
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.purple]
-        navigationController?.navigationBar.backgroundColor = .white
-        navigationController?.navigationBar.barTintColor = .white
-        navigationController?.navigationBar.isTranslucent = false
-        
-        navigationItem.backBarButtonItem?.tintColor = .black
-        
-    }
-    
-    func setupScrollView(){
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(scrollView)
-        
-        scrollView.addSubview(contentView)
-        scrollView.addSubview(rulesTitleLabel)
-        scrollView.addSubview(rulesHelpLabel)
-        scrollView.addSubview(categoriesTitleLabel)
-        scrollView.addSubview(categoriesHelpLabel)
-        scrollView.addSubview(categoriesContainer)
-        
-        categoriesContainer.addSubview(category)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        scrollView.isPagingEnabled = true
         
         NSLayoutConstraint.activate([
-            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
-            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            
-            rulesTitleLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            rulesTitleLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
-            
-            rulesHelpLabel.topAnchor.constraint(equalTo: rulesTitleLabel.bottomAnchor, constant: 16),
-            rulesHelpLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            rulesHelpLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8),
-            rulesHelpLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -8),
-            
-            categoriesTitleLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            categoriesTitleLabel.topAnchor.constraint(equalTo: rulesHelpLabel.bottomAnchor, constant: 16),
-            
-            categoriesHelpLabel.topAnchor.constraint(equalTo: categoriesTitleLabel.bottomAnchor, constant: 16),
-//            categoriesHelpLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
-            categoriesHelpLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-            categoriesHelpLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
-            
-            categoriesContainer.topAnchor.constraint(equalTo: categoriesHelpLabel.bottomAnchor, constant: 16),
-            categoriesContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-            categoriesContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
-            categoriesContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
-        
-            category.topAnchor.constraint(equalTo: categoriesContainer.bottomAnchor, constant: 16),
-            category.leadingAnchor.constraint(equalTo: categoriesContainer.leadingAnchor, constant: 16),
-            category.trailingAnchor.constraint(equalTo: categoriesContainer.trailingAnchor, constant: -16),
-            category.bottomAnchor.constraint(equalTo: categoriesContainer.bottomAnchor, constant: 16)
-        
-            
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+        
+        var lastView: UIView?
+        
+        var lastContainerView: UIView?
+        
+        for i in 0...1 {
+            let containerView = UIView()
+            containerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            for row in model.rows {
+                let row = make(rowFor: row)
+                containerView.addSubview(row)
+                
+                NSLayoutConstraint.activate([
+                    row.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                    row.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                    row.topAnchor.constraint(equalTo: lastView?.bottomAnchor ?? containerView.topAnchor) // label.bottom
+                ])
+                
+                lastView = row
+            }
+            
+            scrollView.addSubview(containerView)
+            
+            NSLayoutConstraint.activate([
+                containerView.leadingAnchor.constraint(equalTo: lastContainerView?.trailingAnchor ?? scrollView.contentLayoutGuide.leadingAnchor),
+                containerView.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor), // label.bottom
+                containerView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+                containerView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor) // label.bottom
+            ])
+            
+            lastContainerView = containerView
+            lastView = nil
+            
+            NSLayoutConstraint.activate([
+                containerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
+            ])
+            
+            if i == 1, let lastContainerView {
+                NSLayoutConstraint.activate([
+                    lastContainerView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor)
+                ])
+            }
+        }
+    }
+    
+    func make(rowFor viewModel: RulesCellViewModel) -> UIView {
+        let container = UIView(frame: .zero)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        
+        switch viewModel {
+        case let .imageAndText(image, text):
+            let label = UILabel(frame: .zero)
+            label.text = text
+            label.numberOfLines = 0
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = .boldSystemFont(ofSize: 22)
+            label.textAlignment = .center
+            
+            let imageView = UIImageView(frame: .zero)
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.contentMode = .scaleAspectFill
+            
+            container.addSubview(label)
+            container.addSubview(imageView)
+            
+            NSLayoutConstraint.activate([
+                imageView.topAnchor.constraint(equalTo: container.topAnchor),
+                imageView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+                imageView.heightAnchor.constraint(equalToConstant: 60),
+                imageView.widthAnchor.constraint(equalToConstant: 60),
+                label.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
+                label.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -8),
+                label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor),
+                label.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+            ])
+            
+            guard let image else { break }
+            
+            imageView.image = image
+        case .title(let text):
+            let label = UILabel(frame: .zero)
+            label.text = text
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = .boldSystemFont(ofSize: 40)
+            label.textAlignment = .center
+            label.textColor = UIColor(named: "purple")
+            container.addSubview(label)
+            
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: container.topAnchor),
+                label.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 8),
+                label.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+                label.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            ])
+            
+        case .image(let image):
+            let imageView = UIImageView(frame: .zero)
+            imageView.image = image
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.contentMode = .scaleAspectFill
+            
+            container.addSubview(imageView)
+            NSLayoutConstraint.activate([
+                imageView.topAnchor.constraint(equalTo: container.topAnchor),
+                imageView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+                imageView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+                imageView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
+            ])
+        }
+        
+        return container
     }
 }
 
--- BOMB/RulesViewContoller/Model/RulesModel.swift
+++ BOMB/RulesViewContoller/Model/RulesModel.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 struct RulesModel {
-    let titleRules: String?
-    let rulesLabel: String?
-    let startButtonImage: UIImage?
-    let titleCategories: String?
-    let categoriesLabel: String?
-    let categoriesImageView: UIImageView?
+    let rows: [RulesCellViewModel] = [
+        .title("Правила Игры"),
+        .imageAndText(UIImage(named: "1"), "Все игроки становятся в круг."),
+        .imageAndText(UIImage(named: "2"), "Первый игрок берет телефон и нажимает кнопку:"),
+        .image(UIImage(named: "start-game-rules") ?? UIImage()),
+        .imageAndText(UIImage(named: "3"), "На экране появляется вопрос. Например, «Назовите фрукт»."),
+        .imageAndText(UIImage(named: "4"), "Игрок отвечает на вопрос и после правильного ответа передает телефон следующему игроку (правильность ответа определяют другие) участники"),
+        .imageAndText(UIImage(named: "5"), "Игроки по кругу отвечают на один и тот же вопрос до тех пор, пока не взорвется бомба."),
+        .imageAndText(UIImage(named: "6"), "Проигравшим считается тот, в чьих руках взорвалась бомба."),
+        .imageAndText(UIImage(named: "7"), "Если в настройках выбран режим игры «С заданиями», то проигравший выполняет задание.")
+    ]
 }

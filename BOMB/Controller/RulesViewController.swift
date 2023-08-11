4972252ffed751aabb760146b7413037ea10eb0d
Daria Sotnikova <dasha.dasha5454@yandex.ru>
2023-08-11 17:45:41 +0300

    add categories

--- BOMB/Controller/RulesViewController.swift
+++ BOMB/Controller/RulesViewController.swift
@@ -13,43 +13,18 @@ class RulesViewController: UIViewController {
     let contentView = UIView()
     let model = RulesModel()
     
-        lazy var rulesTitleLabel: UILabel = {
-            let label = UILabel(frame: .zero)
-            label.text = "Правила Игры"
-            label.textColor = UIColor(named: "purple")
-            label.font = .boldSystemFont(ofSize: 40)
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
-    
-        lazy var starButtonImage: UIImageView = {
-            let image = UIImageView(frame: .zero)
-            image.translatesAutoresizingMaskIntoConstraints = false
-            image.image = UIImage(named: "start-game-rules")
-            return image
-        }()
-    
-        lazy var categoriesTitleLabel: UILabel = {
-            let label = UILabel(frame: .zero)
-            label.text = "Категории"
-            label.textColor = UIColor(named: "purple")
-            label.font = .boldSystemFont(ofSize: 40)
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
-   
-        func configureNavBar() {
-            navigationItem.title = "Помощь"
-            navigationController?.navigationBar.barStyle = .default
-//            navigationController?.navigationBar.prefersLargeTitles = true
-            navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.purple]
-            navigationController?.navigationBar.backgroundColor = .white
-            navigationController?.navigationBar.barTintColor = .white
-            navigationController?.navigationBar.isTranslucent = false
-    
-            navigationItem.backBarButtonItem?.tintColor = .black
-    
-        }
+    func configureNavBar() {
+        navigationItem.title = "Помощь"
+        navigationController?.navigationBar.barStyle = .default
+        //            navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.purple]
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+        
+        navigationItem.backBarButtonItem?.tintColor = .black
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,57 +40,101 @@ class RulesViewController: UIViewController {
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+//            scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
-        var lastView: UIView?
+        createRulesScreen()
+        createCategoriesScreen()
+    }
+    
+    
+    func createRulesScreen() {
         
+        var lastView: UIView?
         var lastContainerView: UIView?
         
-        for i in 0...1 {
-            let containerView = UIView()
-            containerView.translatesAutoresizingMaskIntoConstraints = false
-            
-            for row in model.rulesPageRows {
-                let row = make(rowFor: row)
-                containerView.addSubview(row)
-                
-                NSLayoutConstraint.activate([
-                    row.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-                    row.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-                    row.topAnchor.constraint(equalTo: lastView?.bottomAnchor ?? containerView.topAnchor) // label.bottom
-                ])
-                
-                lastView = row
-            }
-            
-            scrollView.addSubview(containerView)
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        for row in model.rulesPageRows {
+            let row = make(rowFor: row)
+            containerView.addSubview(row)
             
             NSLayoutConstraint.activate([
-                containerView.leadingAnchor.constraint(equalTo: lastContainerView?.trailingAnchor ?? scrollView.contentLayoutGuide.leadingAnchor),
-                containerView.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor), // label.bottom
-                containerView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-                containerView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor) // label.bottom
+                row.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                row.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                row.topAnchor.constraint(equalTo: lastView?.bottomAnchor ?? containerView.topAnchor)
             ])
             
-            lastContainerView = containerView
-            lastView = nil
-            
-            NSLayoutConstraint.activate([
-                containerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
-            ])
-            
-            if i == 1, let lastContainerView {
-                NSLayoutConstraint.activate([
-                    lastContainerView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor)
-                ])
-            }
+            lastView = row
         }
+        
+        scrollView.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: lastContainerView?.trailingAnchor ?? scrollView.contentLayoutGuide.leadingAnchor),
+            containerView.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor)
+        ])
+        
+        lastContainerView = containerView
+        lastView = nil
+        
+        NSLayoutConstraint.activate([
+            containerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func createCategoriesScreen() {
+        var lastView: UIView?
+        
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        for row in model.categoriesPageRows {
+            let row = make(rowFor: row)
+            containerView.addSubview(row)
+            
+            NSLayoutConstraint.activate([
+                row.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                row.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                row.topAnchor.constraint(equalTo: lastView?.bottomAnchor ?? containerView.topAnchor)
+            ])
+            
+            lastView = row
+        }
+        
+        scrollView.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: scrollView.subviews.first?.trailingAnchor ?? scrollView.contentLayoutGuide.leadingAnchor),
+            containerView.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor)
+        ])
+    
+        lastView = nil
+        
+        NSLayoutConstraint.activate([
+            containerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor)
+        ])
     }
     
     func make(rowFor viewModel: RulesCellViewModel) -> UIView {
         let container = UIView(frame: .zero)
         container.translatesAutoresizingMaskIntoConstraints = false
+        
+        let categoryContainerLeft = UIView(frame: .zero)
+        categoryContainerLeft.backgroundColor = UIColor(named: "purple") ?? UIColor()
+        categoryContainerLeft.translatesAutoresizingMaskIntoConstraints = false
+        categoryContainerLeft.layer.cornerRadius = 50
+        
+        let categoryContainerRight = UIView(frame: .zero)
+        categoryContainerRight.backgroundColor = UIColor(named: "purple") ?? UIColor()
+        categoryContainerRight.translatesAutoresizingMaskIntoConstraints = false
+        categoryContainerRight.layer.cornerRadius = 50
         
         switch viewModel {
         case let .imageAndText(image, text):
@@ -147,6 +166,7 @@ class RulesViewController: UIViewController {
             guard let image else { break }
             
             imageView.image = image
+            
         case .title(let text):
             let label = UILabel(frame: .zero)
             label.text = text
@@ -176,6 +196,66 @@ class RulesViewController: UIViewController {
                 imageView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
                 imageView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
             ])
+            
+        case .twoImagesLabel(left: let left, right: let right):
+            let titleLeft = UILabel(frame: .zero)
+            let titleRight = UILabel(frame: .zero)
+            titleLeft.text = left?.0
+            titleRight.text = right?.0
+            titleLeft.textColor = .white
+            titleRight.textColor = .white
+            titleLeft.font = .boldSystemFont(ofSize: 20)
+            titleRight.font = .boldSystemFont(ofSize: 20)
+            titleLeft.textAlignment = .center
+            titleRight.textAlignment = .center
+            titleLeft.numberOfLines = 0
+            titleRight.numberOfLines = 0
+            titleLeft.translatesAutoresizingMaskIntoConstraints = false
+            titleRight.translatesAutoresizingMaskIntoConstraints = false
+            
+            let emojiLeft = UILabel(frame: .zero)
+            let emojiRight = UILabel(frame: .zero)
+            emojiLeft.text = left?.1
+            emojiRight.text = right?.1
+            emojiLeft.font = .boldSystemFont(ofSize: 60)
+            emojiRight.font = .boldSystemFont(ofSize: 60)
+            emojiLeft.translatesAutoresizingMaskIntoConstraints = false
+            emojiRight.translatesAutoresizingMaskIntoConstraints = false
+            
+            container.addSubview(categoryContainerLeft)
+            container.addSubview(categoryContainerRight)
+            categoryContainerLeft.addSubview(titleLeft)
+            categoryContainerLeft.addSubview(emojiLeft)
+            categoryContainerRight.addSubview(titleRight)
+            categoryContainerRight.addSubview(emojiRight)
+            
+            NSLayoutConstraint.activate([
+                categoryContainerLeft.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+                categoryContainerLeft.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.39),
+                categoryContainerLeft.heightAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.39),
+                categoryContainerLeft.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
+                categoryContainerLeft.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 40),
+                
+                emojiLeft.centerXAnchor.constraint(equalTo: categoryContainerLeft.centerXAnchor),
+                emojiLeft.centerYAnchor.constraint(equalTo: categoryContainerLeft.centerYAnchor),
+                
+                titleLeft.centerXAnchor.constraint(equalTo: categoryContainerLeft.centerXAnchor),
+                titleLeft.topAnchor.constraint(equalTo: emojiLeft.bottomAnchor),
+                
+                categoryContainerRight.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+                categoryContainerRight.widthAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.39),
+                categoryContainerRight.heightAnchor.constraint(equalTo: container.widthAnchor, multiplier: 0.39),
+                categoryContainerRight.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
+                categoryContainerRight.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -40),
+                
+                emojiRight.centerXAnchor.constraint(equalTo: categoryContainerRight.centerXAnchor),
+                emojiRight.centerYAnchor.constraint(equalTo: categoryContainerRight.centerYAnchor),
+                
+                titleRight.centerXAnchor.constraint(equalTo: categoryContainerRight.centerXAnchor),
+                titleRight.topAnchor.constraint(equalTo: emojiRight.bottomAnchor)
+            ])
+            
+            categoryContainerRight.isHidden = (titleRight.text ?? "").isEmpty
         }
         
         return container

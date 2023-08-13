c76da4e4cd2a1c91e353248b09f789fe10c6cb82
EliseevDmitry <dimasel@gmail.com>
2023-08-13 15:39:00 +0700

    final without image

--- BOMB/Controller/CategoryViewController.swift
+++ BOMB/Controller/CategoryViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-    
-    var delegate: CheckCategoryProtocol!
+    let heightBtn: CGFloat = 200
+    let widthBtn: CGFloat = 200
+    let btnsHeightText: CGFloat = 25
+var delegate: CheckCategoryProtocol!
+    var buttons: [CustomButton] = []
     
     lazy var background: UIImageView = {
         let backgroundImageView = UIImageView()
         backgroundImageView.frame.size.width = view.frame.width
         backgroundImageView.frame.size.height = view.frame.height
-        backgroundImageView.image = UIImage(named: "background")
+        let background = UIImage(named: "background")
+        backgroundImageView.image = background
+        //self.view.addSubview(backgroundImageView)
         return backgroundImageView
     }()
     
@@ -25,97 +30,264 @@ class CategoryViewController: UIViewController {
         stackVerticalView.distribution = .equalCentering
         stackVerticalView.alignment = .center
         stackVerticalView.spacing = 10
-        stackVerticalView.backgroundColor = .green
+        stackVerticalView.backgroundColor = .clear//free
         stackVerticalView.translatesAutoresizingMaskIntoConstraints = false
         return stackVerticalView
     } ()
     
+    
     lazy var stackHorizViewOne: UIStackView = {
         let stackHorizontalView = UIStackView()
         stackHorizontalView.axis = .horizontal
-        stackHorizontalView.distribution = .equalCentering
-        stackHorizontalView.alignment = .center
-        stackHorizontalView.spacing = 50
-        stackHorizontalView.backgroundColor = .red //free
+        stackHorizontalView.distribution = .fill
+        stackHorizontalView.alignment = .fill
+        stackHorizontalView.spacing = 20
+        stackHorizontalView.backgroundColor = .clear //free
+        //stackHorizontalView.contentMode = .scaleToFill
+        //stackHorizontalView.
         
         //self.view.addSubview(stackHorizontalView)
         return stackHorizontalView
     } ()
-    
     lazy var stackHorizViewTwo: UIStackView = {
         let stackHorizontalView = UIStackView()
         stackHorizontalView.axis = .horizontal
-        stackHorizontalView.distribution = .equalCentering
-        stackHorizontalView.alignment = .center
-        stackHorizontalView.spacing = 50
-        stackHorizontalView.backgroundColor = .green //free
+        stackHorizontalView.distribution = .fill
+        stackHorizontalView.alignment = .fill
+        stackHorizontalView.spacing = 20
+        stackHorizontalView.backgroundColor = .clear //free
         
         //self.view.addSubview(stackHorizontalView)
         return stackHorizontalView
     } ()
-    
     lazy var stackHorizViewThree: UIStackView = {
         let stackHorizontalView = UIStackView()
         stackHorizontalView.axis = .horizontal
-        stackHorizontalView.distribution = .equalCentering
-        stackHorizontalView.alignment = .center
-        stackHorizontalView.spacing = 50
-        stackHorizontalView.backgroundColor = .blue //free
+        stackHorizontalView.distribution = .fill
+        stackHorizontalView.alignment = .fill
+        stackHorizontalView.spacing = 20
+        stackHorizontalView.backgroundColor = .clear //free
         
         //self.view.addSubview(stackHorizontalView)
         return stackHorizontalView
     } ()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addViews(views: background, stackVertView)
-        addViewInStack(stack: stackVertView, views: stackHorizViewOne, stackHorizViewTwo, stackHorizViewThree)
-    }
+    lazy var firstCategoriesBtn: CustomButton = {
+        //тестовая картинка
+        let categoriesOne = CustomButton(categoryImage: "qest", frame: CGRect(x: 1, y: 1, width: 1, height: 1), isPressed: false)
+        categoriesOne.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+//        categoriesOne.frame.size.width = widthBtn
+//        categoriesOne.frame.size.height = heightBtn
+        categoriesOne.layer.cornerRadius = 10
+        categoriesOne.layer.borderWidth = 3.0
+        categoriesOne.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesOne.addTarget(self, action: #selector(categoriesOneAction), for: .touchUpInside)
+        categoriesOne.translatesAutoresizingMaskIntoConstraints = false
+        return categoriesOne
+    }()
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        NSLayoutConstraint.activate([
-            stackVertView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-            stackVertView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-            stackVertView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            //stackVertView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
-        ])
-        NSLayoutConstraint.activate([
-            stackHorizViewOne.topAnchor.constraint(equalTo: stackVertView.topAnchor, constant: 0),
-            stackHorizViewOne.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
-            stackHorizViewOne.trailingAnchor.constraint(equalTo: stackVertView.trailingAnchor, constant: 0),
-            stackHorizViewOne.heightAnchor.constraint(equalToConstant: 200)//delite
-        ])
-        NSLayoutConstraint.activate([
-            stackHorizViewTwo.topAnchor.constraint(equalTo: stackHorizViewOne.bottomAnchor, constant: 0),
-            stackHorizViewTwo.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
-            stackHorizViewTwo.trailingAnchor.constraint(equalTo: stackVertView.trailingAnchor, constant: 0),
-            stackHorizViewTwo.heightAnchor.constraint(equalToConstant: 200)//delite
-        ])
-        NSLayoutConstraint.activate([
-            stackHorizViewThree.topAnchor.constraint(equalTo: stackHorizViewTwo.bottomAnchor, constant: 0),
-            stackHorizViewThree.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
-            stackHorizViewThree.trailingAnchor.constraint(equalTo: stackVertView.trailingAnchor, constant: 0),
-            stackHorizViewThree.heightAnchor.constraint(equalToConstant: 200)//delite
-        ])
-        
-    }
+    lazy var secondCategoriesBtn: CustomButton = {
+        let categoriesTwo = CustomButton(categoryImage: "qest", frame: CGRect(x: 0, y: 0, width: 1, height: 1), isPressed: false)
+        categoriesTwo.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+//        categoriesTwo.frame.size.width = widthBtn
+//        categoriesTwo.frame.size.height = heightBtn
+        categoriesTwo.layer.cornerRadius = 10
+        categoriesTwo.layer.borderWidth = 3.0
+        categoriesTwo.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesTwo.addTarget(self, action: #selector(categoriesTwoAction), for: .touchUpInside)
+        categoriesTwo.translatesAutoresizingMaskIntoConstraints = false
+        return categoriesTwo
+    }()
+    
+    lazy var thirdCategoriesBtn: CustomButton = {
+        //width: 1, height: 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        let categoriesThree = CustomButton(categoryImage: "qest", frame: CGRect(x: 0, y: 0, width: 1, height: 1), isPressed: false)
+        categoriesThree.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+        categoriesThree.frame.size.width = widthBtn
+        categoriesThree.frame.size.height = heightBtn
+        categoriesThree.layer.cornerRadius = 10
+        categoriesThree.layer.borderWidth = 3.0
+        categoriesThree.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesThree.addTarget(self, action: #selector(categoriesThreeAction), for: .touchUpInside)
+        categoriesThree.translatesAutoresizingMaskIntoConstraints = false
+        return categoriesThree
+    }()
+    
+    lazy var fourthCategoriesBtn: CustomButton = {
+        //width: 1, height: 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        let categoriesFour = CustomButton(categoryImage: "qest", frame: CGRect(x: 0, y: 0, width: 1, height: 1), isPressed: false)
+        categoriesFour.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+        categoriesFour.frame.size.width = widthBtn
+        categoriesFour.frame.size.height = heightBtn
+        categoriesFour.layer.cornerRadius = 10
+        categoriesFour.layer.borderWidth = 3.0
+        categoriesFour.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesFour.addTarget(self, action: #selector(categoriesFourAction), for: .touchUpInside)
+        categoriesFour.translatesAutoresizingMaskIntoConstraints = false
+        return categoriesFour
+    }()
+    
+    lazy var fifthCategoriesBtn: CustomButton = {
+        let categoriesFive = CustomButton(categoryImage: "qest", frame: CGRect(x: 0, y: 0, width: 150, height: 150), isPressed: false)
+        categoriesFive.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+        categoriesFive.frame.size.width = widthBtn
+        categoriesFive.frame.size.height = heightBtn
+        categoriesFive.layer.cornerRadius = 10
+        categoriesFive.layer.borderWidth = 3.0
+        categoriesFive.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesFive.addTarget(self, action: #selector(categoriesFiveAction), for: .touchUpInside)
+        categoriesFive.translatesAutoresizingMaskIntoConstraints = false
+        return categoriesFive
+    }()
+    //скрыта
+    lazy var sixthCategoriesBtn: CustomButton = {
+        let categoriesSix = CustomButton(categoryImage: "qest", frame: CGRect(x: 0, y: 0, width: 1, height: 1), isPressed: false)
+        categoriesSix.backgroundColor = UIColor(red: 0x82, green: 0x15, blue: 0xC2)
+        categoriesSix.frame.size.width = widthBtn
+        categoriesSix.frame.size.height = heightBtn
+        categoriesSix.layer.cornerRadius = 10
+        categoriesSix.layer.borderWidth = 3.0
+        categoriesSix.layer.borderColor = UIColor(red: 0x2A, green: 0x14, blue: 0x68).cgColor
+        categoriesSix.addTarget(self, action: #selector(categoriesSixAction), for: .touchUpInside)
+        categoriesSix.alpha = 0
+        return categoriesSix
+    }()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    func addViews(views: UIView...) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addViews(views: background, stackVertView)
+        addViewInStack(stack: stackVertView, views: stackHorizViewOne, stackHorizViewTwo, stackHorizViewThree)
+        addViewInStack(stack: stackHorizViewOne, views: firstCategoriesBtn, secondCategoriesBtn)
+        addViewInStack(stack: stackHorizViewTwo, views: thirdCategoriesBtn, fourthCategoriesBtn)
+        addViewInStack(stack: stackHorizViewThree, views: fifthCategoriesBtn, sixthCategoriesBtn)
+        addArrayButtons()
+    }
+    
+    func addArrayButtons(){
+        buttons.append(firstCategoriesBtn)
+        buttons.append(secondCategoriesBtn)
+        buttons.append(thirdCategoriesBtn)
+        buttons.append(fourthCategoriesBtn)
+        buttons.append(fifthCategoriesBtn)
+    }
+    
+    override func viewWillLayoutSubviews(){
+        super.viewWillLayoutSubviews()
+        NSLayoutConstraint.activate([
+            stackVertView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackVertView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackVertView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            //stackVertView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+        NSLayoutConstraint.activate([
+            stackHorizViewOne.topAnchor.constraint(equalTo: stackVertView.topAnchor, constant: 10),
+            stackHorizViewOne.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
+            stackHorizViewOne.trailingAnchor.constraint(equalTo: stackVertView.trailingAnchor, constant: 0),
+            //stackHorizViewOne.heightAnchor.constraint(equalToConstant: 160)//delite
+        ])
+        NSLayoutConstraint.activate([
+            stackHorizViewTwo.topAnchor.constraint(equalTo: stackHorizViewOne.bottomAnchor, constant: 20),
+            stackHorizViewTwo.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
+            stackHorizViewTwo.trailingAnchor.constraint(equalTo: stackVertView.trailingAnchor, constant: 0),
+            stackHorizViewTwo.heightAnchor.constraint(equalTo: stackHorizViewOne.heightAnchor)
+            
+            //stackHorizViewTwo.heightAnchor.constraint(equalToConstant: 150)//delite
+        ])
+        NSLayoutConstraint.activate([
+            stackHorizViewThree.topAnchor.constraint(equalTo: stackHorizViewTwo.bottomAnchor, constant: 20),
+            stackHorizViewThree.leadingAnchor.constraint(equalTo: stackVertView.leadingAnchor, constant: 0),
+            stackHorizViewThree.heightAnchor.constraint(equalTo: stackHorizViewTwo.heightAnchor)
+        ])
+        //stackHorizViewOne
+        NSLayoutConstraint.activate([
+            fifthCategoriesBtn.widthAnchor.constraint(equalTo: firstCategoriesBtn.heightAnchor),
+            secondCategoriesBtn.widthAnchor.constraint(equalTo: secondCategoriesBtn.heightAnchor),
+        ])
+        
+        //stackHorizViewTwo
+        NSLayoutConstraint.activate([
+            thirdCategoriesBtn.widthAnchor.constraint(equalTo: thirdCategoriesBtn.heightAnchor),
+            fourthCategoriesBtn.widthAnchor.constraint(equalTo: fourthCategoriesBtn.heightAnchor),
+        ])
+        //stackHorizViewThree
+        NSLayoutConstraint.activate([
+            fifthCategoriesBtn.widthAnchor.constraint(equalTo: fifthCategoriesBtn.heightAnchor),
+            sixthCategoriesBtn.widthAnchor.constraint(equalTo: sixthCategoriesBtn.heightAnchor),
+        ])
+        for button in buttons {
+            button.setCheckMarkValue()
+        }
+    }
+    
+
+    
+    
+    
+    func addViews(views: UIView...){
         for view in views {
             self.view.addSubview(view)
         }
     }
     
-    func addViewInStack(stack: UIStackView, views: UIView...) {
+    func addViewInStack(stack: UIStackView, views: UIView...){
         for view in views {
             stack.addArrangedSubview(view)
         }
     }
+    
+    func deleteCheckMark(sender: CustomButton){
+        for button in buttons {
+                if button.isPressed {
+                    button.isPressed = !button.isPressed
+                    button.setChekMark()
+                }
+        }
+        sender.isPressed = !sender.isPressed
+        sender.setChekMark()
+    }
+    
+    @objc func categoriesOneAction(){
+        deleteCheckMark(sender: self.firstCategoriesBtn)
+        print("firstCatigory")
+    }
+    
+    @objc func categoriesTwoAction(){
+        deleteCheckMark(sender: self.secondCategoriesBtn)
+        print("SecondCatigory")
+    }
+    
+    @objc func categoriesThreeAction(){
+        deleteCheckMark(sender: self.thirdCategoriesBtn)
+        print("ThirdCatigory")
+    }
+    
+    @objc func categoriesFourAction(){
+        deleteCheckMark(sender: self.fourthCategoriesBtn)
+        print("FourthCatigory")
+    }
+    
+    @objc func categoriesFiveAction(){
+        deleteCheckMark(sender: self.fifthCategoriesBtn)
+        print("FifthCatigory")
+    }
+    
+    @objc func categoriesSixAction(){
+        print("SixthCatigory")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
+
---
+++ BOMB/Extentional/CustomButton.swift
@@ -0,0 +1,79 @@
+//
+//  CustomButton.swift
+//  TestAPP
+//
+//  Created by Максим Горячкин on 11.08.2023.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    lazy var checkMarkImage: UIImageView = {
+        let checkMark = UIImageView()
+        return checkMark
+    }()
+    
+    var isPressed: Bool
+    
+    init(categoryImage: String, frame: CGRect, isPressed: Bool) {
+        self.isPressed = isPressed
+        super.init(frame: frame)
+        self.addSubview(checkMarkImage)
+        self.setImage(UIImage(named: categoryImage), for: .normal)
+        setCheckMarkValue()
+        setChekMark()
+    }
+    
+    func setCheckMarkValue(){
+        checkMarkImage.frame.size.width = self.frame.width/6
+        checkMarkImage.frame.size.height = self.frame.height/6
+        checkMarkImage.frame.origin.x = self.layer.cornerRadius/2+1
+        checkMarkImage.frame.origin.y = self.layer.cornerRadius/2+1
+    }
+ 
+    func setChekMark(){
+        if isPressed {
+            self.checkMarkImage.image = UIImage(systemName: "checkmark.square")
+        } else {
+            self.checkMarkImage.image = UIImage(systemName: "square")
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func touchIn(){
+        isPressed = !isPressed
+        self.setChekMark()
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       options: [.allowUserInteraction, .curveEaseIn],
+                       animations: {
+            self.transform = .init(scaleX: 0.95, y: 0.95)
+        },
+                       completion: nil)
+    }
+    
+    func touchEnd(){
+        
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       options: [.allowUserInteraction, .curveEaseOut],
+                       animations: {
+            self.transform = .identity
+        },
+                       completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        touchIn()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        touchEnd()
+    }
+}
